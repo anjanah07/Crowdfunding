b0270e7e8226ea450e9a416788ec99d300c5a44a
anjanah07 <thanjana7@gmail.com>
2024-09-19 23:30:41 +0530

    add: contract add & render allCampaigns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
 "use client";
 
+import { getContract } from "thirdweb";
+import { client } from "./client";
+import { sepolia } from "thirdweb/chains";
+import { CROWDFUNDING_FACTORY } from "./constants/contract";
+import { useReadContract } from "thirdweb/react";
+
 export default function Home() {
+  const contract = getContract({
+    client: client,
+    chain: sepolia,
+    address: CROWDFUNDING_FACTORY,
+  });
+
+  const { data: campaigns, isLoading } = useReadContract({
+    contract,
+    method:
+      "function getAllCampaigns() view returns ((address campaignAddress, address owner, string name, uint256 creationTime)[])",
+    params: [],
+  });
+  console.log(campaigns);
   return (
     <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
       <div className="py-10">
         <h1 className="text-4xl font-bold mb-4">Campaigns: </h1>
+        <div className="grid grid-cols-3 gap-4">
+          {!isLoading &&
+            campaigns &&
+            (campaigns?.length > 0 ? (
+              <div>
+                {campaigns.map((campaign, index) => (
+                  <div key={index}>
+                    <p>{campaign.name}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>No Campaigns</div>
+            ))}
+        </div>
       </div>
     </main>
   );
